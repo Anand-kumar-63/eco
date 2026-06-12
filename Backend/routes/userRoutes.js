de25bfad737b4161bf4ddcdefe77af3d9c997d60
Anand-Kumar-63 <anandks8013@gmail.com>
2026-06-12 22:57:37 +0530

    User Router updated

--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,10 +1,10 @@
 import express from "express"
-import { userLogin , userSignup , userUpdate , getUser } from "../controller/authController.js";
+import { userLogin , userSignup , getUser } from "../controller/authController.js";
 const router = express.Router();
 
 router.post("/signup", userSignup);
 router.post("/login", userLogin);
 router.get("/get", getUser);
-router.post("/update" , userUpdate);
 
+// router.post("/update" , userUpdate);
 export default router;
