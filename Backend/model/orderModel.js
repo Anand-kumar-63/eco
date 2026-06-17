cc1238610c5516348918da9d5dff49fa8c2c8ebd
Anand-Kumar-63 <anandks8013@gmail.com>
2026-06-18 01:19:38 +0530

    Minor Changes

--- Backend/model/orderModel.js
+++ Backend/model/orderModel.js
@@ -1,32 +1,32 @@
 import mongoose from "mongoose";
 const orrderSchema = new mongoose.Schema({
-    userId: {
+    userId:{
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
         required: true
     },
-    items: [
+    items:[
         {
             productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
             quantity: { type: Number, required: true, min: 1 },
             price: { type: Number, required: true }
         }
     ],
-    totalAmount: { type: Number, required: true },
-    address: {
+    totalAmount:{type: Number, required: true },
+    address:{
         fullName: { type: String, required: true },
         street: { type: String, required: true },
         city: { type: String, required: true },
         postalCode: { type: String, required: true },
         country: { type: String, required: true }
     },
-    PaymentId: { type: String, required: true },
+    PaymentId:{ type: String, required: true },
     status:{
         type:String,
         enum:["shipped","delivered","pending"],
         default:"pending"
     }
-}, { timestamps: true });
+},{ timestamps: true });
 
 const OrderModel = mongoose.model("Order",orrderSchema);
 export default OrderModel;
